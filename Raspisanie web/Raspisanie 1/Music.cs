39f2ea68b037927a5595728cb468ed604cfe7348
feelgood easytrain <feelgood@bitbucket.org>
2011-04-27 22:23:50 +0400

    ??? ???????? ?????? ????????? ?????????? ????? web. ? ???? ????? ?????? ???????  ?? ??????????.

---
+++ Raspisanie web/Raspisanie 1/Music.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Media;
+
+
+namespace Raspisanie
+{
+    public class Music
+    {
+        [DllImport("winmm.dll")]
+        public static extern long mciSendString(string strCommand, StringBuilder strReturn,
+        int iReturnLength, IntPtr hwndCallback);
+
+        private string sCommand = ""; 
+
+        public void music_on()
+        {
+            sCommand = "open \"" + @"Voodoo People .mp3" + "\" type mpegvideo alias MediaFile";
+            //посылаем команду
+            mciSendString(sCommand, null, 0, IntPtr.Zero);
+            //команда для воспроизведения файла
+            sCommand = "play MediaFile";
+            //посылаем команду
+            mciSendString(sCommand, null, 0, IntPtr.Zero);
+            
+        }
+
+        public void music_off()
+        {
+            mciSendString("stop MediaFile", null, 0, IntPtr.Zero);
+            //// mciSendString("pause MediaFile", null, 1, IntPtr.Zero);
+        }
+
+    }
+}
