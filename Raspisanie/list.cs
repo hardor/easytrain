9bb3ccf8c708026314acf54641cf39b9018fcb47
feelgood easytrain <hardorzz@gmail.com>
2011-04-27 21:55:57 +0400

    tfrfdgdfgdfgdfgdfgd?f??????

---
+++ Raspisanie/list.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Raspisanie
+{
+    public class Lists 
+    {
+        public void list()
+        {
+            ListView listVw = new ListView();
+                     
+            ListViewItem lvi = new ListViewItem();
+            ListViewItem.ListViewSubItem lvsi;
+            ListViewItem.ListViewSubItem lvsisi;
+            //-----------------------------
+            lvi = new ListViewItem();
+            lvsi = new ListViewItem.ListViewSubItem();
+            lvsisi = new ListViewItem.ListViewSubItem();
+            lvi.Text = "1";
+            lvsi.Text = "Свеча горела";
+            lvsisi.Text = "Алла Пугачёва";
+            lvi.SubItems.Add(lvsisi);
+            lvi.SubItems.Add(lvsi);                     
+            listVw.Items.Add(lvi);
+            //-----------------------------
+            lvi = new ListViewItem();
+            lvsi = new ListViewItem.ListViewSubItem();
+            lvsisi = new ListViewItem.ListViewSubItem();
+            lvi.Text = "2";
+            lvsi.Text = "Ты кидал";
+            lvsisi.Text = "Пятница";
+            lvi.SubItems.Add(lvsisi);
+            lvi.SubItems.Add(lvsi);
+            listVw.Items.Add(lvi);
+        }
+
+    }
+}
---
+++ Raspisanie/parse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace Raspisanie
+{
+    class parse
+    {
+        public string list(string uristr)
+        {            
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uristr);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            Stream istrm = resp.GetResponseStream();
+            StreamReader rdr = new StreamReader(istrm);
+            string str = rdr.ReadToEnd();
+            return str;
+        }       
+       
+    }
+}
