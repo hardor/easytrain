91cc91c586001a95c2dfb633e8dcc416d433e329
feelgood <feelgood@localhost>
2011-04-29 23:49:30 +0400

    ????????? ??????????. ????????? ? ??????? ???????. ?????????? ??????????? ????????.

--- Raspisanie web/Raspisanie 1/listview.cs
+++ Raspisanie web/Raspisanie 1/listview.cs
@@ -10,14 +10,15 @@ using System.Windows.Forms;
 using Microsoft.Win32;
 
 
-namespace Raspisanie 
+namespace Raspisanie
 {
-    class listViewZapolnenie 
+    class listViewZapolnenie
     {
+        strwork_elec el_str = new strwork_elec();
 
         public void lists(ListView listView1, Label label, TextBox elec_otpr, TextBox elec_nazn, TextBox elec_data)
         {
-         listView1.Items.Clear();
+            listView1.Items.Clear();
 
             ListViewItem lv1 = new ListViewItem();
             ListViewItem.ListViewSubItem lv2;
@@ -25,37 +26,35 @@ namespace Raspisanie
             ListViewItem.ListViewSubItem lv4;
             ListViewItem.ListViewSubItem lv5;
             ListViewItem.ListViewSubItem lv6;
+            label.Visible = true;
+            label.Text = el_str.zagolovok(elec_otpr.Text, elec_nazn.Text, elec_data.Text);
 
-            
-           
+
             int end;
-            for (end = 1; end <4; end++)
+            for (end = 1; end < 10; end++)
             {
-                strwork_elec el_str = new strwork_elec();
-                label.Visible = true;
-                label.Text = el_str.zagolovok(1, 1, elec_otpr.Text, elec_nazn.Text, elec_data.Text);        
-
                 lv1 = new ListViewItem();
                 lv2 = new ListViewItem.ListViewSubItem();
                 lv3 = new ListViewItem.ListViewSubItem();
                 lv4 = new ListViewItem.ListViewSubItem();
                 lv5 = new ListViewItem.ListViewSubItem();
-                lv6 = new ListViewItem.ListViewSubItem();               
+                lv6 = new ListViewItem.ListViewSubItem();
 
                 lv1.Text = Convert.ToString(end);
-                lv2.Text = el_str.zagolovok(2, end, elec_otpr.Text, elec_nazn.Text, elec_data.Text); 
-                lv3.Text = "10 45";
-                lv4.Text = "12 00";
-                lv5.Text = "2 45 ";
+                lv2.Text = el_str.table(1, end, elec_otpr.Text, elec_nazn.Text, elec_data.Text);
+                lv3.Text = el_str.table(2, end, elec_otpr.Text, elec_nazn.Text, elec_data.Text);
+                lv4.Text = el_str.table(3, end, elec_otpr.Text, elec_nazn.Text, elec_data.Text);
+
+                //lv5.Text = Convert.ToString(Convert.ToDouble(lv4.Text) - Convert.ToDouble(lv3.Text));
                 lv6.Text = "Все";
-                
+
 
                 lv1.SubItems.Add(lv2);
                 lv1.SubItems.Add(lv3);
                 lv1.SubItems.Add(lv4);
                 lv1.SubItems.Add(lv5);
                 lv1.SubItems.Add(lv6);
-                
+
                 listView1.Items.Add(lv1);
             }
         }
--- Raspisanie web/Raspisanie 1/strwork_elec.cs
+++ Raspisanie web/Raspisanie 1/strwork_elec.cs
@@ -13,43 +13,49 @@ using Microsoft.Win32;
 
 namespace Raspisanie
 {
-    
+
 
     public class strwork_elec
     {
-        int[] begin_reis = new int[10], sered_reis = new int[10], end_reis = new int[10];
-        
-        public string zagolovok(int vid, int number,string otprv, string nazn, string date)
+        int[] begin_reis = new int[100], sered_reis = new int[100], end_reis = new int[100];
+        int[] begin_otpr = new int[100], end_otpr = new int[100];
+        int[] begin_pr = new int[100], end_pr = new int[100];
+
+        string s_el;
+        parse parse_el = new parse();
+
+
+        public string zagolovok(string otprv, string nazn, string date)
         {
-            string s_el;
-            parse parse_el = new parse();
             //s_el = parse_el.list("http://rasp.yandex.ru/search/suburban/?fromName=" + otprv + "&fromId=&toName=" + nazn + "&&toId=&when=" + date);
             s_el = parse_el.list("http://rasp.yandex.ru/search/suburban/?fromName=" + "Марк" + "&fromId=&toName=" + "Новодачная" + "&&toId=&when=" + "29.04");
 
-            if ((vid == 1) && (number==1))  //заголовок
-            {
-                string begin = "<title>";
-                string end = "</title>";
+            string begin = "<title>";
+            string end = "</title>";
 
-                int begin_n = s_el.IndexOf(begin);
-                int end_n = s_el.IndexOf(end);
+            int begin_n = s_el.IndexOf(begin);
+            int end_n = s_el.IndexOf(end);
 
-                string zag = s_el.Substring(begin_n + begin.Length, end_n - begin_n - begin.Length);
+            string zag = s_el.Substring(begin_n + begin.Length, end_n - begin_n - begin.Length);
 
-                string begin_mon = "<strong><span>";
-                string end_mon_r = "&nbsp;р";
+            string begin_mon = "<strong><span>";
+            string end_mon_r = "&nbsp;р";
 
-                int begin_mon1 = s_el.IndexOf(begin_mon);
-                int end_mon_r1 = s_el.IndexOf(end_mon_r);
+            int begin_mon1 = s_el.IndexOf(begin_mon);
+            int end_mon_r1 = s_el.IndexOf(end_mon_r);
 
-                string zena = s_el.Substring(begin_mon1 + begin_mon.Length, end_mon_r1 - begin_mon1 - begin_mon.Length);
+            string zena = s_el.Substring(begin_mon1 + begin_mon.Length, end_mon_r1 - begin_mon1 - begin_mon.Length);
 
-                zag = zag + "     Стоимость проезда " + zena + " рублей";
-                return zag;
-            }
+            zag = zag + "     Стоимость проезда " + zena + " рублей";
+            return zag;
 
+        }
 
-            if (vid == 2)//рейсы
+        public string table(int vid, int number, string otprv, string nazn, string date)
+        {
+            //s_el = parse_el.list("http://rasp.yandex.ru/search/suburban/?fromName=" + otprv + "&fromId=&toName=" + nazn + "&&toId=&when=" + date);
+            s_el = parse_el.list("http://rasp.yandex.ru/search/suburban/?fromName=" + "Марк" + "&fromId=&toName=" + "Новодачная" + "&&toId=&when=" + "29.04");
+            if (vid == 1)//рейсы
             {
                 string begin = @"<div class=""b-timetable__tripname_type_print""><span class=""g-nowrap"">";
                 string sered = @"</span> — <span class=""g-nowrap"">";
@@ -59,13 +65,54 @@ namespace Raspisanie
                 sered_reis[0] = 0;
                 end_reis[0] = 0;
 
-                begin_reis[number] = s_el.IndexOf(begin, end_reis[number - 1]);
-                sered_reis[number] = s_el.IndexOf(sered, begin_reis[number]);
-                end_reis[number] = s_el.IndexOf(end, sered_reis[number]);
+                if (s_el.IndexOf(begin, end_reis[number - 1]) == -1)
+                {
+                    return "No";
+                }
 
-                string reiss = Convert.ToString(begin_reis[number])+"  " + s_el.Substring(begin_reis[number] + begin.Length, sered_reis[number] - begin_reis[number] - begin.Length) + " - " +
-                    s_el.Substring(sered_reis[number] + sered.Length, end_reis[number] - sered_reis[number] - sered.Length);
-                return reiss;
+                else
+                {
+                    begin_reis[number] = s_el.IndexOf(begin, end_reis[number - 1]);
+
+                    sered_reis[number] = s_el.IndexOf(sered, begin_reis[number]);
+                    end_reis[number] = s_el.IndexOf(end, sered_reis[number]);
+
+                    string reiss = s_el.Substring(begin_reis[number] + begin.Length, sered_reis[number] - begin_reis[number] - begin.Length) + " - " +
+                        s_el.Substring(sered_reis[number] + sered.Length, end_reis[number] - sered_reis[number] - sered.Length);
+
+                    return reiss;
+                }
+            }
+
+            if (vid == 2)//время отправления
+            {
+                string begin = @"', 'col': 'dep'}""><strong>";
+                begin_otpr[0] = 0;
+
+
+                begin_otpr[number] = s_el.IndexOf(begin, begin_otpr[number - 1] +1);
+
+                string otpr = s_el.Substring(begin_otpr[number] + begin.Length, 5);
+
+                return otpr;
+
+            }
+
+
+            if (vid == 3)//время прибытия
+            {
+                string begin = @", 'col': 'arr'}""><strong>";
+
+                begin_pr[0] = 0;
+
+
+                begin_pr[number] = s_el.IndexOf(begin, begin_pr[number - 1] + 1);
+
+
+                string pr = s_el.Substring(begin_pr[number] + begin.Length, 5);
+
+                return pr;
+
             }
             else { return "Warning"; }
 
