7efa94b734ada3924e13c316db7731cb76ccba8b
Nikita_Nikitin_guru_linux <Nikita_Nikitin_guru_linux@localhost>
2011-05-05 22:48:40 +0400

    ??????? ?????????? ??? ????????? ? ???????

--- Raspisanie web/Raspisanie 1/avia/strwork_avia.cs
+++ Raspisanie web/Raspisanie 1/avia/strwork_avia.cs
@@ -1,120 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Text;
 using System.Windows.Forms;
 using Microsoft.Win32;
 
-//1)заголовок и стоимость
-//2)рейсы
+
 
 namespace Raspisanie
 {
 
-
     public class strwork_avia
     {
-        int[] begin_reis = new int[100], sered_reis = new int[100], end_reis = new int[100];
-        int[] begin_otpr = new int[100], end_otpr = new int[100];
-        int[] begin_pr = new int[100], end_pr = new int[100];
+        int[] begin_reis = new int[100], sered_reis = new int[100], sered_reis2 = new int[100], end_reis = new int[100];
+        int[] begin_otpr = new int[100], end_otpr = new int[100], otshet_otpr = new int[100];
+        int[] begin_pr = new int[100], end_pr = new int[100], otshet_pr = new int[100];
+        int[] begin_zena = new int[100], end_zena = new int[100];
 
-        string s_el;
-        parse parse_el = new parse();
+        ListViewItem lv1 = new ListViewItem();
+        ListViewItem.ListViewSubItem lv2;
+        ListViewItem.ListViewSubItem lv3;
+        ListViewItem.ListViewSubItem lv4;
+        ListViewItem.ListViewSubItem lv5;
 
+        string s_avia;
+        parse parse_avia = new parse();
 
         public string zagolovok(string otprv, string nazn, string date)
         {
-            //s_el = parse_el.list("http://rasp.yandex.ru/search/suburban/?fromName=" + otprv + "&fromId=&toName=" + nazn + "&&toId=&when=" + date);
-            s_el = parse_el.list("http://rasp.yandex.ru/search/suburban/?fromName=" + "Марк" + "&fromId=&toName=" + "Новодачная" + "&&toId=&when=" + "29.04");
+            s_avia = parse_avia.list(" http://rasp.yandex.ru/search/plane/?fromName=" +otprv + "&fromId=&toName=" + nazn + "&&toId=&when=" + date);
+           // s_avia = parse_avia.list(" http://rasp.yandex.ru/search/plane/?fromName=" + "Москва" + "&fromId=&toName=" + "Уфа" + "&&toId=&when=" + date);
+            if ((s_avia.IndexOf("у нас нет информации") > 0) || (s_avia.IndexOf("нет данных") > 0))
+            {
+                return "Нет информации";
+            }
+            else
+            {
+                string begin = "<title>";
+                string end = "</title>";
 
-            string begin = "<title>";
-            string end = "</title>";
-
-            int begin_n = s_el.IndexOf(begin);
-            int end_n = s_el.IndexOf(end);
-
-            string zag = s_el.Substring(begin_n + begin.Length, end_n - begin_n - begin.Length);
-
-            string begin_mon = "<strong><span>";
-            string end_mon_r = "&nbsp;р";
-
-            int begin_mon1 = s_el.IndexOf(begin_mon);
-            int end_mon_r1 = s_el.IndexOf(end_mon_r);
-
-            string zena = s_el.Substring(begin_mon1 + begin_mon.Length, end_mon_r1 - begin_mon1 - begin_mon.Length);
-
-            zag = zag + "     Стоимость проезда " + zena + " рублей";
-            return zag;
+                int begin_n = s_avia.IndexOf(begin);
+                int end_n = s_avia.IndexOf(end);
+                string zag = s_avia.Substring(begin_n + begin.Length, end_n - begin_n - begin.Length);
+                return zag;
+            }
 
         }
 
-        public string table(int vid, int number, string otprv, string nazn, string date)
+        public void table(ListView listView3, string otprv, string nazn, string date)
         {
-            //s_el = parse_el.list("http://rasp.yandex.ru/search/suburban/?fromName=" + otprv + "&fromId=&toName=" + nazn + "&&toId=&when=" + date);
-            s_el = parse_el.list("http://rasp.yandex.ru/search/suburban/?fromName=" + "Марк" + "&fromId=&toName=" + "Новодачная" + "&&toId=&when=" + "29.04");
-            if (vid == 1)//рейсы
+             s_avia = parse_avia.list("http://rasp.yandex.ru/search/plane/?fromName=" + otprv + "&fromId=&toName=" + nazn + "&&toId=&when=" + date);
+           //s_avia = parse_avia.list(" http://rasp.yandex.ru/search/plane/?fromName=" + "Москва" + "&fromId=&toName=" + "Уфа" + "&&toId=&when=" + date);
+
+            int number = 1;
+            
+            while (1 > 0)
             {
-                string begin = @"<div class=""b-timetable__tripname_type_print""><span class=""g-nowrap"">";
-                string sered = @"</span> — <span class=""g-nowrap"">";
-                string end = @"</span></div>";
+                lv1 = new ListViewItem();
+                lv2 = new ListViewItem.ListViewSubItem();
+                lv3 = new ListViewItem.ListViewSubItem();
+                lv4 = new ListViewItem.ListViewSubItem();
+                lv5 = new ListViewItem.ListViewSubItem();
 
-                begin_reis[0] = 0;
-                sered_reis[0] = 0;
-                end_reis[0] = 0;
+                lv1.Text = Convert.ToString(number);
 
-                if (s_el.IndexOf(begin, end_reis[number - 1]) == -1)
-                {
-                    return "No";
-                }
 
-                else
-                {
-                    begin_reis[number] = s_el.IndexOf(begin, end_reis[number - 1]);
+                string begin_1 = @"<div class=""b-timetable__description"">" + "\n            ";
+                string sered_1 = @", <a class=""b-link"" href=""/info/company/";
+                string sered_2 = @""">";
+                string end_1 = @"</a>";
 
-                    sered_reis[number] = s_el.IndexOf(sered, begin_reis[number]);
-                    end_reis[number] = s_el.IndexOf(end, sered_reis[number]);
+                begin_reis[number] = s_avia.IndexOf(begin_1, end_reis[number - 1]);
+                if (begin_reis[number] < 0) { break; }
+                sered_reis[number] = s_avia.IndexOf(sered_1, begin_reis[number]);
+                sered_reis2[number] = s_avia.IndexOf(sered_2, sered_reis[number]);
+                end_reis[number] = s_avia.IndexOf(end_1, sered_reis2[number]);
+                lv2.Text = s_avia.Substring(begin_reis[number] + begin_1.Length, sered_reis[number] - begin_reis[number] - begin_1.Length) + "  " + s_avia.Substring(sered_reis2[number] + sered_2.Length, end_reis[number] - sered_reis2[number] - sered_2.Length);
 
-                    string reiss = s_el.Substring(begin_reis[number] + begin.Length, sered_reis[number] - begin_reis[number] - begin.Length) + " - " +
-                        s_el.Substring(sered_reis[number] + sered.Length, end_reis[number] - sered_reis[number] - sered.Length);
 
-                    return reiss;
-                }
+                string otshet_2 = @"<td class=""b-timetable__column b-timetable__column_type_departure b-timetable__column_state_current"">";
+                string begin_2 = @"}, 'local': '";
+                string end_2 = @"', 'col': 'dep'}""><strong>";
+                otshet_otpr[number] = s_avia.IndexOf(otshet_2, end_otpr[number - 1]);
+                begin_otpr[number] = s_avia.IndexOf(begin_2, otshet_otpr[number]);
+                end_otpr[number] = s_avia.IndexOf(end_2, begin_otpr[number]);
+                lv3.Text = s_avia.Substring(begin_otpr[number] + begin_2.Length, end_otpr[number] - begin_otpr[number] - begin_2.Length);
+
+                string otshet_3 = @"<td class=""b-timetable__column b-timetable__column_type_arrival"">" + "\n";
+                string begin_3 = @"}, 'local': '";
+                string end_3 = @"', 'col': 'arr'}""><strong>";
+                otshet_pr[number] = s_avia.IndexOf(otshet_3, end_pr[number - 1]);
+                begin_pr[number] = s_avia.IndexOf(begin_3, otshet_pr[number]);
+                end_pr[number] = s_avia.IndexOf(end_3, begin_pr[number]);
+                lv4.Text = s_avia.Substring(begin_pr[number] + begin_3.Length, end_pr[number] - begin_pr[number] - begin_3.Length);
+
+
+                //string begin_4 = @"<div class=""b-price js-subsort {raw: ";
+                // string end_4 = @", name: '";              
+                //begin_zena[number] = s_avia.IndexOf(begin_4, end_reis[number]);             
+                
+                //if (begin_zena[number] < 0) { break; }
+                
+                // end_zena[number] = s_avia.IndexOf(end_4, begin_zena[number]);
+                // if (end_zena[number] < 0) { break; }
+                //lv5.Text = s_avia.Substring(begin_zena[number] + begin_4.Length, end_zena[number] - begin_zena[number] - begin_4.Length);
+
+
+
+                
+                lv1.SubItems.Add(lv2);
+                lv1.SubItems.Add(lv3);
+                lv1.SubItems.Add(lv4);
+                lv1.SubItems.Add(lv5);
+
+
+                listView3.Items.Add(lv1);
+                number += 1;
             }
-
-            if (vid == 2)//время отправления
-            {
-                string begin = @"', 'col': 'dep'}""><strong>";
-                begin_otpr[0] = 0;
-
-
-                begin_otpr[number] = s_el.IndexOf(begin, begin_otpr[number - 1] +1);
-
-                string otpr = s_el.Substring(begin_otpr[number] + begin.Length, 5);
-
-                return otpr;
-
-            }
-
-
-            if (vid == 3)//время прибытия
-            {
-                string begin = @", 'col': 'arr'}""><strong>";
-
-                begin_pr[0] = 0;
-
-
-                begin_pr[number] = s_el.IndexOf(begin, begin_pr[number - 1] + 1);
-
-
-                string pr = s_el.Substring(begin_pr[number] + begin.Length, 5);
-
-                return pr;
-
-            }
-            else { return "Warning"; }
 
         }
 
